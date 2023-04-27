a2471915b4c7c16950a8450443d3fa29503615ac
Valeria Belova <bairelav@gmail.com>
2023-04-27 11:01:29 +0200

    first commit

---
+++ src/main/java/com/example/backend1webshop/Backend1WebshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend1webshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backend1WebshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Backend1WebshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backend1webshop/Backend1WebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend1webshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backend1WebshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

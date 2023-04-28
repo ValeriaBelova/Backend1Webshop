b692723238ab37a24860cac41b9f0eec03fb0eb5
RobinBroberg <robin.broberg86@gmail.com>
2023-04-28 12:20:32 +0200

    Product controller work

--- src/main/java/com/example/backend1webshop/Webshop/controllers/ProductController.java
+++ src/main/java/com/example/backend1webshop/Webshop/controllers/ProductController.java
@@ -1,7 +1,31 @@
 package com.example.backend1webshop.Webshop.controllers;
 
+import com.example.backend1webshop.Webshop.models.Product;
+import com.example.backend1webshop.Webshop.repos.ProductRepo;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ProductController {
+
+    private final ProductRepo repo;
+
+    public ProductController(ProductRepo repo) {
+        this.repo = repo;
+    }
+
+    @RequestMapping("items")
+    public List<Product> getAllProducts(){
+        return repo.findAll();
+    }
+
+    @RequestMapping("/items/{id}")
+    public Optional<Product> itemsById(@PathVariable Long id){
+
+        return repo.findById(id);
+    }
 }
